af09cdb40d224d121239f7bf7e8bd3102e4a40d6
David <whited9339@gmail.com>
2017-03-07 23:27:28 -0700

    Trying to make it work

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = 'd739d37b250a4deb902355e3e4bbb32d'; // Your client id
 var client_secret = '9d16385d5beb4eb9a5a65b5b27391b8a'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://localhost:5000/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -143,5 +143,5 @@ app.get('/refresh_token', function(req, res) {
 
 
 
-console.log('Listening on 8888');
-app.listen(8888);
+console.log('Listening on 5000');
+app.listen(5000);
