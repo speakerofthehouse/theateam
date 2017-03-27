950d4ea0d8d8cb1a2383507180c6021dd6febd67
Eric Speaker <ersp4587@colorado.edu>
2017-03-27 12:53:02 -0600

    Update algorithm.c

--- algorithm.c
+++ algorithm.c
@@ -13,12 +13,6 @@ algorithm functions
 
 */
 
-array base_list; //parameters: [number(number in the list),song_name,artist,album,ranking]
-
-
-queue shuffle_edit; //same parameters
-
-
 int i = 0;
 void basic_play(queue playlist){ //void??
 	while(playlist != is_empty){
@@ -29,6 +23,31 @@ void basic_play(queue playlist){ //void??
 	}
 }
 
+void basic_shuffle(queue playlist){
+	/*create the shuffle*/
+	//assuming queue element starts at 0 and increments --- so song 1 is q[0]
+	//currentsong = playlist[0];
+	//nextsong = playlist[1];
+	array b_s_list = [];
+
+	int x = 0;	//bslist spot
+	int p_len = sizeof(playlist);	
+	int y = p_len;	//oldlist counter
+	while(y != 0){
+		lv = rand() % p_len; //pick a random number from the playlist to add into the queue
+		/*add song and "remove it from the old list*/
+		b_s_list[x] = playlist[lv];
+		while(int z = lv; z < p_len; z++){
+			playlist[z] = playlist[z+1];
+		}
+		x++;
+		y--;
+		p_len--;		
+	}
+ 	/*run the shuffle*/
+		--inside or outside the function??? -- main??
+}
+
 
 
 
@@ -36,6 +55,19 @@ void main(){
 
 	/*create queue/array */
 	/*create editable queue*/	
+	
+	array base_list; //parameters: [number(number in the list),song_name,artist,album,ranking]
+
+
+	array shuffle_edit; //same parameters
+	l_size = sizeof(base_list);
+	
+	/*b/c of sizing and how preallocation i am initially setting things to size of original list*/
+	/*will have to be developed for what happens when a person adds a new song while running a list*/
+	array b_s_list[l_size];
+	array b_shuff[l_size];
+	array r_shuff[5*l_size]; //5 b/c of rankings from 1-5 and how that would affect algorithm 
+	array al_shuff[l_size];
 
 	int userinput = 0;
 	cin << shuff_type;
@@ -66,8 +98,4 @@ void main(){
 	}
 		
 }
-
-
-
-
 
