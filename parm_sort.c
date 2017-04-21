3bcdc13dfadbeafff3558eab10e6884f73163f94
Eric Speaker <ersp4587@colorado.edu>
2017-04-21 17:58:01 -0400

    Add files via upload

---
+++ parm_sort.c
@@ -0,0 +1,53 @@
+//pad_array
+padArray(array,numpads){
+	remander = numpads;
+	var temp = [max];
+	for(i=0;i<array_len;i++ ){
+		temp[i] = i
+	}
+	while(remainder != 0){
+		temp[i].push("dummy")
+		remainder --;
+	}
+	return temp;
+}
+
+//merge_array
+mege(arrays){
+	//loop through each artist array
+	var fin_array = [orig_len];
+	for(i=0;i<max_len;i++){
+		for(j=0;j<array_length;j++){
+			if(j[i] != "dummy" ){
+				fin_array[].push();
+			}
+		}
+	}
+	return fin_array;
+}
+
+//spread shuffle
+spreadshuffle(){
+	var arrays = [orig_len];
+	orig = ...spotify array....
+	for(t=0;t<orig_len;t++){
+		if(){	//artist name array exists
+			//stuff		
+		}
+		else{
+			//create the new array element
+		}
+	}
+	max = findMaxLength(arrays);
+	for(i=0;i<array_len;i++){
+		if(array[i].lengh<max){
+			array[i] = padArray(array[i],(max-arrays[i].length))		
+		}
+		randshuffle(array[i]);
+	}
+	fin_array = merge(arrays)
+
+//spotify api calls for fin_array
+
+
+}
